328a5d6cedf3d057e153fa84ae00551fb0abf08d
Jiiks <jiiks@windowslive.com>
2015-11-09 15:05:05 +0200

    init

---
+++ JMemLib/ObjectWrapper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+/**
+ *   JMemLib ObjectWrapper
+ *   https://github.com/Jiiks/JMemLib
+ *
+ *   Copyright (c) 2015 Jiiks
+ *   
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *   
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+namespace JMemLib
+{
+    //ObjectWrapper supports Primitives, String and byte[]
+    public class ObjectWrapper : IComparable<Object>
+    {
+        private Object _value;
+        public Object GetValue()
+        {
+            return _value;
+        }
+
+        public void SetValue(Object value)
+        {
+            if (CompareTo(value) < 1) return;
+
+            _value = value;
+            OnPropertyChanged();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null)handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public int CompareTo(Object obj)
+        {
+            if (obj == null || _value == null)
+            {
+                return -1;
+            }
+
+            Type type = obj.GetType();
+
+            if (type != _value.GetType() || !type.IsPrimitive || typeof (String) != type || typeof (byte[]) != type)
+            {
+                return -1;
+            }
+
+            if (type.IsPrimitive || typeof (String) == type)
+            {
+                return obj.Equals(_value) ? 1 : 0;
+            }
+
+            byte[] oldVal = _value as byte[];
+            byte[] newVal = obj as byte[];
+
+            if (oldVal == null || newVal == null)
+            {
+                return -1;
+            }
+
+            return oldVal.SequenceEqual(newVal) ? 1 : 0;
+        }
+
+    }
+}
---
+++ JMemLib/Pointer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+
+/**
+ *   JMemLib Pointer
+ *   https://github.com/Jiiks/JMemLib
+ *
+ *   Copyright (c) 2015 Jiiks
+ *   
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *   
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+namespace JMemLib
+{
+    public class Pointer
+    {
+
+        private int _address;
+        private IntPtr _handle;
+        private bool _alive, _stopped;
+
+        public Pointer(Int32 address, IntPtr handle, bool alive = true)
+        {
+            _address = address;
+            _handle = handle;
+            _alive = alive;
+        }
+
+        public byte[] ReadByteArray(int bufferSize, int offset)
+        {
+            return ReadByteArray(bufferSize, new[] {offset});
+        }
+
+        public byte[] ReadByteArray(int bufferSize, int[] offsets)
+        {
+            int bytesRead = 0;
+            byte[] buffer = new byte[bufferSize];
+
+            Native.ReadProcessMemory((int) _handle, _address, buffer, buffer.Length, ref bytesRead);
+
+            if (offsets.Length < 1)
+            {
+                return buffer;
+            }
+
+            if (offsets.Length < 2)
+            {
+                Native.ReadProcessMemory((int) _handle, _address + offsets[0], buffer, buffer.Length, ref bytesRead);
+                return buffer;
+            }
+
+            int tAddress = BitConverter.ToInt32(buffer, 0);
+
+            foreach (int offset in offsets)
+            {
+                Native.ReadProcessMemory((int)_handle, tAddress + offset, buffer, buffer.Length, ref bytesRead);
+                tAddress = BitConverter.ToInt32(buffer, 0);
+            }
+
+            return buffer;
+        }
+
+        public void ReadListener(int bufferSize, int[] offsets,bool isBackground, ObjectWrapper objectWrapper)
+        {
+            Thread t = new Thread(() =>
+            {
+                while (!_stopped)
+                {
+
+                    if (_alive)
+                    {
+                        byte[] bytes = ReadByteArray(bufferSize, offsets);
+                        objectWrapper.SetValue(bytes);
+                    }
+                    else
+                    {
+                        Thread.Sleep(100);
+                    }
+
+                    Thread.Sleep(10);
+
+                }
+            }) { IsBackground = isBackground };
+            t.Start();
+        }
+
+        public void Pause()
+        {
+            _alive = false;
+        }
+
+        public void Resume()
+        {
+            _alive = true;
+        }
+
+        public void Stop()
+        {
+            _stopped = true;
+        }
+    }
+}
